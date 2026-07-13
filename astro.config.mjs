68185fcf29806bf9d3f24371dfd424e9c3066cf2
curiocatStudio <curiocatstudio@gmail.com>
2026-07-13 14:36:44 +0700

    Update game page and image browser

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://curiocatstudio.com',
+  site: 'https://curiocat-studio.vercel.app',
+
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
+    sitemap(),
   ],
 });
